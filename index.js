fc68ff97766878c3c45b9c43fd322b052db6e753
Raymond An <raymond_an@yahoo.com>
2023-02-04 23:53:53 -0500

    solution

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+const hqLocation = 42
+
+function distanceFromHqInBlocks(location) {
+    return Math.abs(location - hqLocation)
+}
+
+
+function distanceFromHqInFeet(value) {
+    return distanceFromHqInBlocks(value)*264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination)*264
+  }
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) < 400) {
+        return 0
+    } else if (distanceTravelledInFeet(start, destination) < 2000) {
+        return (distanceTravelledInFeet(start, destination)- 400 )*0.02
+    } else if (distanceTravelledInFeet(start, destination) < 2500) {
+        return 25
+    } else {
+        return 'cannot travel that far'
+    }
+  }
